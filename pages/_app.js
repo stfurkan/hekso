75ac5dffc06da57814ed9c20fa2910ef8e81274a
Sait Furkan Teke <35101659+stfurkan@users.noreply.github.com>
2022-06-20 20:58:49 -0400

    add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import { SessionProvider } from 'next-auth/react';
+import Router from 'next/router';
+import nProgress from 'nprogress';
 
 import SEO from '../components/SEO';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 import '../styles/globals.css';
+import '../styles/nprogress.css';
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
 
 function MyApp({ Component, pageProps }) {
   return (
